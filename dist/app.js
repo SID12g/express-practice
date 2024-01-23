ead871f7bcd9395a25c18d83b938a31f699c3c7d
SID12g <sid12g@naver.com>
2024-01-23 22:39:40 +0900

    feat: add post

--- dist/app.js
+++ dist/app.js
@@ -2,10 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var app = express();
-var port = 8000;
-app.get("/", function (req, res) {
+var port = 3000;
+app.get("/test", function (req, res) {
     console.log(req);
     res.send({ hello: "world" });
+});
+app.post("/test", function (req, res) {
+    res.send({ cat: "cheese" });
 });
 app.listen(port, function () {
     console.log("Example app listening at http://localhos:".concat(port));
--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,15 @@ import * as express from "express";
 
 const app: express.Express = express();
 
-const port: number = 8000;
+const port: number = 3000;
 
-app.get("/", (req: express.Request, res: express.Response) => {
+app.get("/test", (req: express.Request, res: express.Response) => {
   console.log(req);
   res.send({ hello: "world" });
+});
+
+app.post("/test", (req, res) => {
+  res.send({ cat: "cheese" });
 });
 
 app.listen(port, () => {
